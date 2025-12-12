e61a0f1b5b7a1c16b0ce1aaab1fbdfbe072921c3
stefan-mouope <stefan.bouognong@facsciences-uy1.cm>
2025-12-13 00:39:32 +0100

    test des config pour les bd

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require("sequelize");
 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS ? "****" : process.env.DB_PASS);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,       // tarabia_db
-  process.env.DB_USER,       // tarabia_db_user
-  process.env.DB_PASS,       // password
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,  // dpg-xxxx
-    port: process.env.DB_PORT,  // 5432
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "postgres",
     logging: false,
     dialectOptions: {
